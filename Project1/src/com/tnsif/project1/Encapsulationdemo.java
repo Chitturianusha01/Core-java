7eb98779ac41eff05bf8042d9424a35a098b2487
Chitt <Chitt@DESKTOP-0OF7LMD>
2023-09-23 14:42:01 +0530

    second commit

---
+++ Project1/src/com/tnsif/project1/Encapsulationdemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.project1;
+
+public class Encapsulationdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation obj = new Encapsulation();
+		obj.serialno=1;
+		obj.name="anu";
+		obj.age=20;
+		obj.show();
+		
+
+	}
+
+}

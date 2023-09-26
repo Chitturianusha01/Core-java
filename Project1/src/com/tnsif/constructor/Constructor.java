9286c2f17851822d3db202fa4d2b1c892e8cadf8
Chitt <Chitt@DESKTOP-0OF7LMD>
2023-09-26 19:50:21 +0530

    five commit

---
+++ Project1/src/com/tnsif/constructor/Constructor.java
@@ -0,0 +1,7 @@
+package com.tnsif.constructor;
+
+public class Constructor {
+	private String name;
+	private 
+
+}
---
+++ Project1/src/com/tnsif/inheritance/Superdemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.inheritance;
+
+public class Superdemo {
+	int var=50;
+	void display() {
+		System.out.println("the value is "+var);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Superdemo ob= new Superdemo();
+		ob.display();
+
+	}
+
+}

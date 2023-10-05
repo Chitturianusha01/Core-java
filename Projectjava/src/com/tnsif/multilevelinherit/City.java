e8736989aa239a3574aeebdc6be2102e2b1ffa03
Chitt <Chitt@DESKTOP-0OF7LMD>
2023-10-05 20:37:09 +0530

    FOURTH COMMIT

---
+++ Projectjava/src/com/tnsif/multilevelinherit/City.java
@@ -0,0 +1,20 @@
+package com.tnsif.multilevelinherit;
+
+public class City {
+	private String Cityname;
+
+	public String getCityname() {
+		return Cityname;
+	}
+
+	public void setCityname(String cityname) {
+		Cityname = cityname;
+	}
+
+	@Override
+	public String toString() {
+		return "City [Cityname=" + Cityname + "]";
+	}
+	
+
+}
---
+++ Projectjava/src/com/tnsif/multilevelinherit/Country.java
@@ -0,0 +1,16 @@
+package com.tnsif.multilevelinherit;
+
+public class Country {
+	private String countryname;
+
+	public String getCountryname() {
+		return countryname;
+	}
+
+	public void setCountryname(String countryname) {
+		this.countryname = countryname;
+	}
+
+	
+
+}

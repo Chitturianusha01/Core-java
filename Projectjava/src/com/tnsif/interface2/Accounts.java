b5a72b937c8715048c2daeaa6a1bf1de13358ee7
Chitt <Chitt@DESKTOP-0OF7LMD>
2023-10-05 21:44:17 +0530

    TWELVETH COMMIT

---
+++ Projectjava/src/com/tnsif/interface2/Accounts.java
@@ -0,0 +1,8 @@
+package com.tnsif.interface2;
+
+public interface Accounts {
+void deliveryCharges(int amt) ;//abstract method;
+
+void deliveryCharges();
+
+}
---
+++ Projectjava/src/com/tnsif/interface2/NonPrime.java
@@ -0,0 +1,19 @@
+package com.tnsif.interface2;
+
+public class NonPrime implements Accounts {
+
+	@Override
+	public void deliveryCharges() {
+		// TODO Auto-generated method stub
+	int	amt=100;
+	System.out.println("The delivery charge for nonprime account is" +amt);
+	}
+
+	@Override
+	public void deliveryCharges(int amt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}

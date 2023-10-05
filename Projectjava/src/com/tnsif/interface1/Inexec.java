4a0b7c1fc7461f06c98afba7833489c07490eb6e
Chitt <Chitt@DESKTOP-0OF7LMD>
2023-10-05 21:11:53 +0530

    TENTH DEMO

---
+++ Projectjava/src/com/tnsif/interface1/Inexec.java
@@ -0,0 +1,12 @@
+package com.tnsif.interface1;
+
+public class Inexec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Indemo i= new Indemo();
+        i.show();
+        i.print();
+	}
+
+}
---
+++ Projectjava/src/com/tnsif/interface1/InterfaceOne.java
@@ -0,0 +1,5 @@
+package com.tnsif.interface1;
+
+public interface InterfaceOne {
+	void print();
+}
